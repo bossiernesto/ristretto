865b7c28f3519f01cff89c9e3da6a449f8381049
bossiernesto <bossi.ernestog@gmail.com>
2014-07-04 14:28:28 -0300

    make AsyncHTTPServer compatible with python 3.4+

--- testServer/SimpleAsyncHTTPServer.py
+++ testServer/SimpleAsyncHTTPServer.py
@@ -43,73 +43,72 @@ tended to hang below 10% processor utilization.
 
 There exists a live host running this web server: nada.ics.uci.edu
 """
-
+#TODO: refactor to clean code and make tests
+import codecs
 import asynchat
 import asyncore
 import socket
-import SimpleHTTPServer
+import http.server
 import sys
 import cgi
-import cStringIO
+import io
 import os
 import traceback
 import zlib
 import optparse
-import urlparse
+import urllib.parse
 from collections import deque
 
 __version__ = ".2"
+
+
+def encode(s, name, *args, **kwargs):
+    codec = codecs.lookup(name)
+    print(codec)
+    rv, length = codec.encode(s, *args, **kwargs)
+    if not isinstance(rv, (str, bytes, bytearray)):
+        raise TypeError('Not a string or byte codec')
+    return rv
+
+
+def popall(deque):
+    #Preallocate the list to save memory resizing.
+    r = len(deque) * [None]
+    for i in range(len(r)):
+        r[i] = deque.popleft()
+    return r
 
 
 class writewrapper(object):
     def __init__(self, d, blocksize=4096):
         self.blocksize = blocksize
         self.d = d
+
     def write(self, data):
         if self.blocksize in (None, -1):
             self.d.append(data)
         else:
             BS = self.blocksize
             xtra = 0
-            if len(data)%BS:
-                xtra = len(data)%BS + BS
+            if len(data) % BS:
+                xtra = len(data) % BS + BS
             buf = self.d
-            for i in xrange(0, len(data)-xtra, BS):
-                buf.append(data[i:i+BS])
+            for i in range(0, len(data) - xtra, BS):
+                buf.append(data[i:i + BS])
             if xtra:
                 buf.append(data[-xtra:])
-#
-class ParseHeaders(dict):
-    """Replacement for the deprecated mimetools.Message class
-    Works like a dictionary with case-insensitive keys"""
 
-    def __init__(self, infile, *args):
-        self._ci_dict = {}
-        lines = infile.readlines()
-        for line in lines:
-            k,v=line.split(":",1)
-            self._ci_dict[k.lower()] = self[k] = v.strip()
-        self.headers = self.keys()
 
-    def getheader(self, key, default=""):
-        return self._ci_dict.get(key.lower(), default)
+class RequestHandler(asynchat.async_chat, http.server.SimpleHTTPRequestHandler):
+    server_version = "SimpleAsyncHTTPServer/" + __version__
+    protocol_version = "HTTP/1.1"
+    blocksize = 4096
 
-    def get(self, key, default=""):
-        return self._ci_dict.get(key.lower(), default)
-#
-
-class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHandler):
-    if 1:
-        server_version = "SimpleAsyncHTTPServer/"+__version__
-        protocol_version = "HTTP/1.1"
-        MessageClass = ParseHeaders
-        blocksize = 4096
-
-        #In enabling the use of buffer objects by setting use_buffer to True,
-        #any data block sent will remain in memory until it has actually been
-        #sent.
-        use_buffer = False
-        use_favicon = True
+    #In enabling the use of buffer objects by setting use_buffer to True,
+    #any data block sent will remain in memory until it has actually been
+    #sent.
+    use_buffer = False
+    use_favicon = False
 
     def __init__(self, conn, addr, server):
         asynchat.async_chat.__init__(self, conn)
@@ -119,7 +118,7 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
         # set the terminator : when it is received, this means that the
         # http request is complete ; control will be passed to
         # self.found_terminator
-        self.set_terminator ('\r\n\r\n')
+        self.set_terminator(bytes('\r\n\r\n', 'UTF-8'))
         self.incoming = deque()
         self.outgoing = deque()
         self.rfile = None
@@ -129,7 +128,8 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
         self.code = None
         # buffer the response and headers to avoid several calls to select()
 
-    def collect_incoming_data(self,data):
+    def collect_incoming_data(self, data):
+
         """Collect the data arriving on the connexion"""
         if not data:
             self.ac_in_buffer = ""
@@ -147,7 +147,7 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
 
     def handle_post_data(self):
         """Called when a POST request body has been read"""
-        self.rfile = cStringIO.StringIO(''.join(deque.popall(self.incoming)))
+        self.rfile = io.BytesIO(b''.join(popall(self.incoming)))
         self.rfile.seek(0)
         self.do_POST()
 
@@ -156,8 +156,8 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
 
         # Check for query string in URL
         qspos = self.path.find('?')
-        if qspos>=0:
-            self.body = urlparse.parse_qs(self.path[qspos+1:], keep_blank_values=1)
+        if qspos >= 0:
+            self.body = urllib.parse.parse_qs(self.path[qspos + 1:], keep_blank_values=1)
             self.path = self.path[:qspos]
 
         self.handle_data()
@@ -171,10 +171,10 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
             self.body = cgi.parse_multipart(self.rfile, pdict)
         elif ctype == 'application/x-www-form-urlencoded':
             qs = self.rfile.read(length)
-            self.body = urlparse.parse_qs(qs, keep_blank_values=1)
+            self.body = urllib.parse.parse_qs(qs, keep_blank_values=1)
         else:
             self.body = ''                   # Unknown content-type
-        #self.handle_post_body()
+            #self.handle_post_body()
         self.handle_data()
 
     def handle_data(self):
@@ -206,21 +206,21 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
     def handle_request_line(self):
         """Called when the http request line and headers have been received"""
         # prepare attributes needed in parse_request()
-        self.rfile = cStringIO.StringIO(''.join(deque.popall(self.incoming)))
+        self.rfile = io.BytesIO(b''.join(popall(self.incoming)))
         self.rfile.seek(0)
         self.raw_requestline = self.rfile.readline()
         self.parse_request()
 
-        if self.command in ['GET','HEAD']:
+        if self.command in ['GET', 'HEAD']:
             # if method is GET or HEAD, call do_GET or do_HEAD and finish
-            method = "do_"+self.command
-            if hasattr(self,method):
-                getattr(self,method)()
-        elif self.command=="POST":
+            method = "do_" + self.command
+            if hasattr(self, method):
+                getattr(self, method)()
+        elif self.command == "POST":
             # if method is POST, call prepare_POST, don't finish yet
             self.prepare_POST()
         else:
-            self.send_error(501, "Unsupported method (%s)" %self.command)
+            self.send_error(501, "Unsupported method (%s)" % self.command)
 
     def end_headers(self):
         """Send the blank line ending the MIME headers, send the buffered
@@ -229,7 +229,7 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
             self.outgoing.append("\r\n")
 
     def handle_error(self):
-        traceback.print_exc(sys.stderr)
+        traceback.print_exc(file=sys.stderr)
         self.close()
 
     def writable(self):
@@ -267,8 +267,13 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
         else:
             #if we get here, the outgoing deque is empty
             return
-        #if we get here, 'a' is a string or buffer object of length > 0
+            #if we get here, 'a' is a string or buffer object of length > 0
         try:
+            if isinstance(a, bytes):
+                a = codecs.decode(a)
+            if isinstance(a, str):
+                a = encode(a, 'utf-8')
+            print('Encoded Data: ', a)
             num_sent = self.send(a)
             if num_sent < len(a):
                 if not num_sent:
@@ -276,14 +281,14 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
                     # allocations of buffers when they are enabled
                     O.appendleft(a)
                 elif self.use_buffer:
-                    O.appendleft(buffer(a, num_sent))
+                    O.appendleft(memoryview(a, num_sent))
                 else:
                     O.appendleft(a[num_sent:])
 
-        except socket.error, why:
-            if isinstance(why, (str, unicode)):
+        except socket.error as why:
+            if isinstance(why, str):
                 self.log_error(why)
-            elif isinstance(why, tuple) and isinstance(why[-1], (str, unicode)):
+            elif isinstance(why, tuple) and isinstance(why[-1], str):
                 self.log_error(why[-1])
             else:
                 self.log_error(str(why))
@@ -298,13 +303,13 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
         if os.path.isdir(path):
             if not self.path.endswith('/'):
                 self.send_response(302)
-                x = '<META HTTP-EQUIV="refresh" CONTENT="0;URL=%s/">'%self.path
+                x = '<META HTTP-EQUIV="refresh" CONTENT="0;URL=%s/">' % self.path
                 self.send_header("Content-Location", self.path + '/')
                 self.send_header("Content-Length", len(x))
                 self.end_headers()
                 self.wfile.write(x)
                 return None
-        return SimpleHTTPServer.SimpleHTTPRequestHandler.send_head(self)
+        return http.server.SimpleHTTPRequestHandler.send_head(self)
 
     def send_response(self, code, message=None):
         if self.code:
@@ -326,11 +331,14 @@ class RequestHandler(asynchat.async_chat, SimpleHTTPServer.SimpleHTTPRequestHand
         sys.stderr.write("%s - - [%s] %s \"%s\" \"%s\"\n" %
                          (self.address_string(),
                           self.log_date_time_string(),
-                          format%args,
+                          format % args,
                           self.headers.get('referer', ''),
                           self.headers.get('user-agent', '')
-                          ))
-#
+                         ))
+
+        #
+
+
 class OnlyExactFiles(RequestHandler):
     def send_head(self):
         path = self.translate_path(self.path)
@@ -338,25 +346,28 @@ class OnlyExactFiles(RequestHandler):
             self.send_error(404, "File not found")
             return None
         return RequestHandler.send_head(self)
-#
+
+        #
+
+
 class ExactFilesAndIndex(RequestHandler):
     def list_directory(self, path):
         self.send_error(404, "File not found")
         return None
-#
+
+        #
+
 
 class Server(asyncore.dispatcher):
-    if 1:
-        """Copied from http_server in medusa"""
-    def __init__ (self, ip, port, handler):
+    def __init__(self, ip, port, handler):
         self.ip = ip
         self.port = port
         self.handler = handler
-        asyncore.dispatcher.__init__ (self)
-        self.create_socket (socket.AF_INET, socket.SOCK_STREAM)
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.set_reuse_addr()
-        self.bind ((ip, port))
+        self.bind((ip, port))
 
         # Quoting the socket module documentation...
         # listen(backlog)
@@ -364,42 +375,23 @@ class Server(asyncore.dispatcher):
         #     specifies the maximum number of queued connections and should
         #     be at least 1; the maximum value is system-dependent (usually
         #     5).
-        self.listen (5)
+        self.listen(5)
 
-    def handle_accept (self):
+    def handle_accept(self):
         try:
             conn, addr = self.accept()
         except socket.error:
-            self.log_info ('warning: server accept() threw an exception', 'warning')
+            self.log_info('warning: server accept() threw an exception', 'warning')
             return
         except TypeError:
-            self.log_info ('warning: server accept() threw EWOULDBLOCK', 'warning')
+            self.log_info('warning: server accept() threw EWOULDBLOCK', 'warning')
             return
-        # creates an instance of the handler class to handle the request/response
+            # creates an instance of the handler class to handle the request/response
         # on the incoming connexion
-        self.handler(conn,addr,self)
+        self.handler(conn, addr, self)
 
-favicon = zlib.decompress(
-'x\x9c\xb5\x93\xcdN\xdb@\x14\x85\x07\x95\x07\xc8\x8amYv\xc9#\xe4\x11x\x04\x96}'
-'\x8c\x88\x1dl\xa0\x9b\xb6A\xa2)\x0bVTB\xa9"\xa5?*I\x16\xad"\x84d\x84DE\x93'
-'\x14;v\xc01M\xe2$\x988\xb1l\x9d\xde;v\\\x03\x89TU\xea\xb5N\xe4\xb9\x9a\xef'
-'\x1c\xcfO\x84X\xa0\'\x95\x12\xf4\xbb,\x9e/\n\xb1$\x84xF\xa2\x16u\xc2>WzQ\xfc'
-'\xf7\xca\xad\xafo\x91T\xd2\x1ai\xe5\x1fx[\xf9\xf4\x01\xc57\xbb\xd8\xdf\xd8'
-'\x00\x8d\x11\xf9\x95\x12\xda\x9a\xc3\xae\xe5_\xbdDpk\x03\xc3\xaeT\xd0\xb3\xd0'
-'>?\x83Z\xfd\x86Z\xa5\x84\x1fG_\xa4\xe7\x1c^\xa9W\xbfJ\xfe\xb4\xf0\x0e^\xdb'
-'\x88}0 \xafA\x0f\xa3+c&O\xbd\xf4\xc1\xf6\xb6d\x9d\xc6\x05\xdcVSz\xb0x\x1c\x10'
-'\x0fo\x02\xc7\xd0\xe7\xf1%\xe5\xf3\xc78\xdb\xf9Y\x93\x1eI\x1f\xf8>\xfa\xb5'
-'\x8bG<\x8dW\x0f^\x84\xd9\xee\xb5~\x8f\xe1w\xaf{\x83\x80\xb2\xbd\xe1\x10\x83'
-'\x88\'\xa5\x12\xbcZ?9\x8e\xb3%\xd3\xeb`\xd4\xd2\xffdS\xb9\x96\x89!}W!\xfb\x9a'
-'\xf9t\xc4f\x8aos\x92\x9dtn\xe0\xe8Z\xcc\xc8=\xec\xf7d6\x97\xa3]\xc2Q\x1b(\xec'
-'d\x99_\x8dx\xd4\x15%\xce\x96\xf9\xbf\xacP\xd1:\xfc\xf1\x18\xbe\xeb\xe2\xaey'
-'\x89;]\xc5\xf1\xfb<\xf3\x99\xe9\x99\xefon\xa2\xdb6\xe5\x1c\xbb^\x8b}FV\x1b'
-'\x9es+\xb3\xbd\x81M\xeb\xd1\xe0^5\xf1\xbd|\xc4\xfca\xf2\xde\xf0w\x9cW\xabr.'
-'\xe7\xd9\x8dFx\x0e\xa6){\x93\x8e\x85\xf1\xb5\x81\x89\xd9\x82\xa1\x9c\xc8;\xf9'
-'\xe0\x0cV\xb8W\xdc\xdb\x83\xa9i\xb1O@g\xa6T*\xd3=O\xeaP\xcc(^\x17\xfb\xe4\xb3'
-'Y\xc9\xb1\x17{N\xf7\xfbo\x8b\xf7\x97\x94\xe3;\xcd\xff)\xd2\xf2\xacy\xa0\x9b'
-'\xd4g=\x11B\x8bT\x8e\x94Y\x08%\x12\xe2q\x99\xd4\x7f*\x84O\xfa\r\xb5\x916R'
-)
+
+favicon = None
 
 which = (RequestHandler, OnlyExactFiles, ExactFilesAndIndex)
 
@@ -407,8 +399,10 @@ which = (RequestHandler, OnlyExactFiles, ExactFilesAndIndex)
 class to_logfile:
     if 1:
         fl = True
+
     def __init__(self, fileobj):
         self.f = fileobj
+
     def write(self, data):
         data = data.encode('utf-8')
         sys.__stdout__.write(data)
@@ -417,7 +411,7 @@ class to_logfile:
             self.f.flush()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     usage = "usage: \%prog -r<root> [-p<port>] [-0|-1|-2]"
 
     parser = optparse.OptionParser(usage)
@@ -445,6 +439,6 @@ if __name__=="__main__":
     os.chdir(options.root)
 
     req_handler = which[options.server]
-    s=Server('', options.port, req_handler)
-    print req_handler.__name__, "running on port", options.port, "with root path", options.root
+    s = Server('', options.port, req_handler)
+    print((req_handler.__name__, "running on port", options.port, "with root path", options.root))
     asyncore.loop()
